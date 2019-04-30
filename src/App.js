15c0d8703a51775fb9e9123f0a239276e74c2d3f
Denis Hasanenko <denis.hasanenko@gmail.com>
2019-04-30 22:29:33 +0300

    add styles

--- src/App.js
+++ src/App.js
@@ -14,26 +14,27 @@ class App extends Component {
   render() {
     return (
         <Router>
-            <div>
+            <header className="container">
+                <div className="logo"><b>Shelter</b>Sub</div>
                 <ul>
                     <li>
-                        <Link to="/">Home</Link>
+                        <Link to="/">Головна</Link>
                     </li>
                     <li>
-                        <Link to="/pets">Pets</Link>
+                        <Link to="/pets">Тварини</Link>
                     </li>
                     <li>
-                        <Link to="/stories">Stories</Link>
+                        <Link to="/stories">Iсторiї</Link>
                     </li>
                     <li>
-                        <Link to="/about">About</Link>
+                        <Link to="/about">Про проект</Link>
                     </li>
                 </ul>
-                <Route exact path='/' component={Home}/>
-                <Route path='/about'   component={About}/>
-                <Route path='/pets'   component={Pets}/>
-                <Route path='/stories'   component={Stories}/>
-            </div>
+            </header>
+            <Route exact path='/' component={Home}/>
+            <Route path='/about'   component={About}/>
+            <Route path='/pets'   component={Pets}/>
+            <Route path='/stories'   component={Stories}/>
         </Router>
     );
   }
--- src/home/HomeComponent.jsx
+++ src/home/HomeComponent.jsx
@@ -9,6 +9,15 @@ function HomeComponent({
     console.log(state);
     return (
         <div>
+            <div className="banner">
+                <div className="container">
+                    <div className="text">
+                        <h2>Не обовязково бути господарем щоб піклуватися</h2>
+                        <p>Зроби внесок за допомогою нашого додатку. Усi зібрани кошти підуть безпосередньо у притулки</p>
+                    </div>
+                    <img src="/images/banner.png" />
+                </div>
+            </div>
             <h1>Home</h1>
             <p>current count: {count}</p>
             <button onClick={onIncrementClick}>Increment</button>
